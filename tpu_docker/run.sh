aa33a00f418eb8a0df85950a112a5e044c2df797
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-07-19 22:32:31 +0800

    add: created sh command

--- tpu_docker/run.sh
+++ tpu_docker/run.sh
@@ -1,4 +1,5 @@
 docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb coral-ubuntu18 /bin/bash
+
 cd tflite/python/examples/classification
 ./install_requirements.sh
 python3 classify_image.py   --model models/mobilenet_v2_1.0_224_inat_bird_quant.tflite   --labels models/inat_bird_labels.txt   --input images/parrot.jpg
